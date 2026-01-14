e5df2df30ac87d11b95f811f881105434fde37e6
MizanRbf <mizanrbf@gmail.com>
2026-01-14 17:27:11 +0600

    query 1 updated

--- queries.sql
+++ queries.sql
@@ -1,19 +1,21 @@
--- Query 1 : JOIN
-SELECT u.name AS customer_name, v.vehicle_name
+SELECT
+  b.id AS booking_id,
+  u.name AS customer_name,
+  v.vehicle_name,
+  b.rent_start_date AS start_date,
+  b.rent_end_date AS end_date,
+  b.status
 FROM bookings b
   INNER JOIN users u ON b.customer_id = u.id
   INNER JOIN vehicles v ON b.vehicle_id = v.id;
 
--- Query 2 : EXISTS
 SELECT * FROM vehicles v
 WHERE NOT EXISTS 
 (SELECT 1 FROM bookings b WHERE b.vehicle_id = v.id);
 
--- Query 3 : WHERE
 SELECT * FROM vehicles
 WHERE type = 'car' AND availability_status = 'available';
 
--- Query 4 : GROUP BY and HAVING
 SELECT v.vehicle_name,
   COUNT(b.vehicle_id) AS total_bookings FROM bookings b 
   JOIN vehicles v ON b.vehicle_id = v.id
